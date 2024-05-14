77a11a73a5437065388b1f14c1aa4d237a238c29
Kevin Per <kevin.per@protonmail.com>
2024-05-14 11:28:13 +0200

    Add login component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'shop', component: AppComponent }
+];
