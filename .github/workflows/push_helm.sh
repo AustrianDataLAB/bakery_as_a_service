52b2c6c2c3285ea80b04cf20890aba2af38c1638
Theo Haslinger <theo@DESKTOP-ANL1A44>
2024-05-28 12:11:42 +0200

    Added publish pipeline for helm charts

---
+++ .github/workflows/push_helm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+cd charts/baas || exit 1
+echo "${REGISTRY_ACCESS_TOKEN}" | helm registry login -u ${REGISTRY_USERNAME} --password-stdin https://${REGISTRY}/${REPOSITORY_NAME}
+helm lint .
+helm package .
+helm inspect chart *.tgz
+helm push *.tgz oci://${REGISTRY}/${REPOSITORY_NAME}
